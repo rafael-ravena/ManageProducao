20b0ad68f7ececa265e4d69f0ba979d2f6785259
rafarave <rafael.ravena@gmail.com>
2014-11-03 17:26:39 -0200

    Casa
    modificando a parada das casas

---
+++ ManageProducao.Api/Controllers/CasaController.cs
@@ -0,0 +1,55 @@
+﻿using ManageProducao.Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ManageProducao.Api.Controllers
+{
+    public class CasaController : ApiController
+    {
+        [HttpGet]
+        [Route("casa/")]
+        public IEnumerable<Casa> Get()
+        {
+            return Casa.GetAll();
+        }
+
+        [HttpGet]
+        [Route("casa/{id}")]
+        public Casa Get(int id)
+        {
+            return Casa.Get(id);
+        }
+
+        [HttpGet]
+        [Route("casa/buscar/{nome}")]
+        public IEnumerable<Casa> Get(string nome)
+        {
+            return Casa.GetAll(nome);
+        }
+
+        [HttpPost]
+        [Route("casa/criar")]
+        public void Create(Casa entity)
+        {
+            Casa.Create(entity);
+        }
+
+        [HttpDelete]
+        [Route("casa/excluir/{id}")]
+        public void Delete(int id)
+        {
+            Casa.Delete(id);
+        }
+
+        [HttpPut]
+        [Route("casa/alterar")]
+        public void Update(Casa entity)
+        {
+            Casa.Update(entity);
+        }
+    }
+}
--- ManageProducao.Api/Model/Casa.cs
+++ ManageProducao.Api/Model/Casa.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ManageProducao.Api.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 
@@ -11,5 +13,39 @@ namespace ManageProducao.Api.Model
 		public string Nome { get; set; }
 		public string Sigla { get; set; }
 		public string Endereco { get; set; }
-	}
+
+        internal static IEnumerable<Casa> GetAll()
+        {
+            return new Context().Casas;
+        }
+        internal static IEnumerable<Casa> GetAll(string nome)
+        {
+            return new Context().Casas.Where(c => c.Nome.ToLower().IndexOf(nome.ToLower()) > -1);
+        }
+        internal static Casa Get(int id)
+        {
+            return new Context().Casas.FirstOrDefault(c => c.Id == id);
+        }
+        internal static void Create(Casa entity)
+        {
+            Context context = new Context();
+            context.Casas.Add(entity);
+            context.SaveChanges();
+        }
+        internal static void Update(Casa entity)
+        {
+            Context context = new Context();
+            Casa original = context.Casas.FirstOrDefault(c => c.Id == entity.Id);
+            context.Entry(original).CurrentValues.SetValues(entity);
+            context.Entry(original).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+        internal static void Delete(int id)
+        {
+            Context context = new Context();
+            Casa original = context.Casas.FirstOrDefault(c => c.Id == id);
+            context.Entry(original).State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+    }
 }
